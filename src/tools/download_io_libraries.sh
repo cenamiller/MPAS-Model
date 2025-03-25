f273b019257be7142ee0538244b63df24b416fae
cenamiller <cena.miller@gmail.com>
2025-03-25 13:51:11 -0600

    adding specific mpich library download for sources

--- src/tools/download_io_libraries.sh
+++ src/tools/download_io_libraries.sh
@@ -5,4 +5,13 @@
 mkdir -p sources
 
 # Use wget to download all files from the directory
+wget -P sources https://www2.mmm.ucar.edu/people/duda/files/mpas/sources/mpich-3.3.1.tar.gz
+
 wget -r -np -nH --cut-dirs=5 -P sources --accept "*.tar.gz" https://www2.mmm.ucar.edu/people/duda/files/mpas/sources/
+
+# Unpack the tarballs
+for file in sources/*.tar.gz; do
+    tar -xzf $file -C sources
+done
+
+
