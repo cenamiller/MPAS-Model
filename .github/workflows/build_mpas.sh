8684e18b11fb0f9549a34152ad6d9371e8fc3824
cenamiller <cena.miller@gmail.com>
2025-07-16 14:43:53 -0600

    MPAS is building, but it's a versions that's downloaded based on tag in script /container/extras/build_mpas.sh . Modifying to build on checked out code.

---
+++ .github/workflows/build_mpas.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+set -ex
+
+#----------------------------------------------------------------------------
+# environment
+SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+source ${SCRIPTDIR}/build_common.cfg || { echo "cannot locate ${SCRIPTDIR}/build_common.cfg!!"; exit 1; }
+#----------------------------------------------------------------------------
+
+# export MPAS_VERSION="${MPAS_VERSION:-8.2.2}"
+
+# cd ${STAGE_DIR}
+# rm -rf ${STAGE_DIR}/*
+
+# curl --retry 3 --retry-delay 5 -sSL https://github.com/MPAS-Dev/MPAS-Model/archive/refs/tags/v${MPAS_VERSION}.tar.gz | tar xz
+# cd ./MPAS-Model-*/
+
+cd ~
+echo "building MPAS from $(pwd)"
+
+export PIO_ROOT=${PIO_ROOT:-/container/pio}
+
+case "${COMPILER_FAMILY}" in
+    "aocc"|"clang")
+        compiler_target="llvm"
+        ;;
+    "gcc")
+        compiler_target="gfortran"
+        ;;
+    "oneapi")
+        compiler_target="intel"
+        ;;
+    "nvhpc")
+        compiler_target="nvhpc"
+        ;;
+    *)
+        echo "ERROR: unrecognized COMPILER_FAMILY=${COMPILER_FAMILY}"!
+        exit 1
+        ;;
+esac
+
+make ${compiler_target} CORE=atmosphere --jobs ${MAKE_J_PROCS:-$(nproc)}
+
+#cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_ROOT}/mpas/${MPAS_VERSION}
+#make --jobs ${MAKE_J_PROCS:-$(nproc)}
+#make install
