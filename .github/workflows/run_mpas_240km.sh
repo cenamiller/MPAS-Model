0f554525ace4a893ab0717d627b9618629f0f36b
cenamiller <cena.miller@gmail.com>
2025-07-18 10:12:56 -0600

    focusing on mpi issues using gnu, no cuda, and smiol for now

--- .github/workflows/run_mpas_240km.sh
+++ .github/workflows/run_mpas_240km.sh
@@ -18,12 +18,17 @@ cd 240km_${NUM_PROCS}/
 ln -sf ../atmosphere_model .
 
 # Modify namelist.atmosphere to change run duration (config_run_duration)
-# from 5 days to half day  
+# from 5 days to quarter day  
 sed -i "s/config_run_duration = '5_00:00:00'/config_run_duration = '0_06:00:00'/" namelist.atmosphere
 
 
 echo "Running MPAS from $(pwd) on $NUM_PROCS processors"
 
 # Run the model
-mpirun -n "$NUM_PROCS" ./atmosphere_model
+# allow-run-as-root is needed for openmpi to run on github actions as root
+if [ "$MPI_IMPL" = "openmpi" ]; then
+    mpirun -n "$NUM_PROCS" --allow-run-as-root ./atmosphere_model
+else
+    mpirun -n "$NUM_PROCS" ./atmosphere_model
+fi
 
