f30a67218c87070a54b606f8df6e45301627a665
cenamiller <cena.miller@gmail.com>
2025-07-25 09:22:01 -0600

    cov test looking for files in nonexistent build dir (from when i tried cmake). Also made run_duration and restart_interval env variables so we can use same script for code cov and run test. Code cov needs REALLY minimal time steps.

--- .github/workflows/run_mpas_240km.sh
+++ .github/workflows/run_mpas_240km.sh
@@ -8,8 +8,13 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${SCRIPTDIR}/build_common.cfg || { echo "cannot locate ${SCRIPTDIR}/build_common.cfg!!"; exit 1; }
 #----------------------------------------------------------------------------
 
-# Accept number of processors as first argument, or use NUM_PROCS env, and default to 1
+# Accept arguments:
+# $1: number of processors (default: 1 or NUM_PROCS env var)
+# $2: run duration in format "DD_HH:MM:SS" (default: RUN_DURATION env var or "0_06:00:00")
+# $3: restart output interval in format "DD_HH:MM:SS" (default: RESTART_INTERVAL env var or "0_06:00:00")
 NUM_PROCS="${1:-${NUM_PROCS:-1}}"
+RUN_DURATION="${2:-${RUN_DURATION:-0_06:00:00}}"
+RESTART_INTERVAL="${3:-${RESTART_INTERVAL:-0_06:00:00}}"
 
 tar xzf .github/workflows/240km.tar.gz
 mv 240km 240km_${NUM_PROCS}
@@ -17,14 +22,16 @@ cd 240km_${NUM_PROCS}/
 
 ln -sf ../atmosphere_model .
 
-# Modify namelist.atmosphere to change run duration (config_run_duration) to 6 hours
-sed -i "s/config_run_duration = '[^']*'/config_run_duration = '0_06:00:00'/" namelist.atmosphere
+# Modify namelist.atmosphere to change run duration (config_run_duration) 
+sed -i "s/config_run_duration = '[^']*'/config_run_duration = '${RUN_DURATION}'/" namelist.atmosphere
 
-# Modify streams.atmosphere to change restart output_interval to 6 hours
-sed -i '/<immutable_stream name="restart"/,/\/>/ s/output_interval="[^"]*"/output_interval="0_06:00:00"/' streams.atmosphere
+# Modify streams.atmosphere to change restart output_interval
+sed -i '/<immutable_stream name="restart"/,/\/>/ s/output_interval="[^"]*"/output_interval="'${RESTART_INTERVAL}'"/' streams.atmosphere
 
 
 echo "Running MPAS from $(pwd) on $NUM_PROCS processors"
+echo "Run duration: $RUN_DURATION"
+echo "Restart interval: $RESTART_INTERVAL"
 echo "MPI_FLAGS: $MPI_FLAGS"
 
 # Run the model with MPI flags
